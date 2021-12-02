30c01120e484ec3997a12f42b8d82c362824b2bf
Michael Moritsugu <mmoritsugu@gmail.com>
2021-12-02 11:53:29 -0800

    Use link-list runners for day2

--- day1/1/notes.md
+++ day1/1/notes.md
@@ -32,3 +32,29 @@ While both still have elements continue
 
 Ran solver
 `racket solv.rkt` and got the correct answer: 1791
+
+expected test-input.txt: 7
+
+# Test input
+```
+199
+200
+208 -> 607
+210 -> 618
+200 -> 618
+207 -> 617
+240 -> 647
+269 -> 716
+260 -> 769
+263 -> 792
+
+607
+618 Increase
+618 
+617
+647 Increase
+716 Increase
+769 Increase
+792 Increase
+
+```
--- day1/2/solv.rkt
+++ day1/2/solv.rkt
@@ -1,38 +1,14 @@
 #lang racket
-(require "../part1/helpers.rkt")
 
-;; # Approach to solution
-;; 1. create triplets of list 
-;; 2. for each triplet create sum
-;; 3. apply same method from part1
-
-
-;; List -> Boolean
-(define (not-null? a-list)
-  (andmap (lambda (element) (not (empty? element))) a-list))
-
-;; Creates a list of triplets for consecutive values of a list.
-;; The first 2 values contain nulls, e.g. <number, nil, nil> and <number, number, nil>
-;; List<Number> -> List<Triplet<Number, Number, Number>>
-(define (build-triplets l1)
-  (define l2 (cons null l1))
-  (define l3 (cons null l2))
-  (define triplets (filter not-null? (zip l1 l2 l3)))
-  (define sums (map (lambda (triplet) (apply + triplet)) triplets))
-  (count-increases sums))
-
-;; Zips 3 lists together
-;; List<Number> List<Number> List<Number -> List<Triplet<Number, Number, Number>>
-(define (zip l1 l2 l3)
-  (if (empty? l1)
-    empty
-    (cons `(,(first l1) ,(first l2) ,(first l3))
-          (zip (rest l1) (rest l2) (rest l3)))))
+(define (count-increases leaving entering count)
+  (cond
+    [(null? entering) count]
+    [(> (first entering) (first leaving)) (count-increases (rest leaving) (rest entering) (add1 count))]
+    [else  (count-increases (rest leaving) (rest entering) count)]))
 
 (define (solver)
   (define lines (map string->number (port->lines (current-input-port))))
-  (define triplets (build-triplets lines))
-  triplets
+  (count-increases lines (list-tail lines 3) 0)
   )
 
 (with-input-from-file "./input.txt"
