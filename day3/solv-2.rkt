1b475266b7358d681693da821f8edb66033b689c
Michael Moritsugu <mmoritsugu@gmail.com>
2021-12-03 19:39:24 -0800

    working refactored version of method

--- day3/solv-2.rkt
+++ day3/solv-2.rkt
@@ -5,61 +5,49 @@
 (define (numbers_10->numbers_2 list-of-numbers)
   (map (lambda (n) (number->string n 2)) list-of-numbers))
 
-(define (oxygen-rating list-of-numbers bit-index)
-  (println (format "called oxygen-rating ~a ~a" list-of-numbers bit-index))
-  (cond [(= (length list-of-numbers) 1) (first list-of-numbers)]
-        [else (begin
-                (define-values (ones zeros) (partition (lambda (n) (bitwise-bit-set? n bit-index))
-                                                       list-of-numbers))
-                (cond
-                  [(= (length ones) (length zeros)) 
-                   (begin
-                     (println (format "recurse on ones: ~a" (numbers_10->numbers_2 ones)))
-                     (oxygen-rating ones (sub1 bit-index)))]
-                  [(> (length ones) (length zeros))
-                   (begin
-                     (println (format "recurse on ones: ~a" (numbers_10->numbers_2 ones)))
-                     (oxygen-rating ones (sub1 bit-index)))]
-                  [(< (length ones) (length zeros))
-                   (begin
-                     (println (format "recurse on zeros ~a" (numbers_10->numbers_2 zeros)))
-                     (oxygen-rating zeros (sub1 bit-index)))]       
+(define (pred-builder bit-index)
+  (lambda (n) (bitwise-bit-set? n bit-index)))
 
-                  ))]
-        ) 
-  )
+(define (rating-builder first-pred second-pred)
+  (lambda (rating list-of-numbers bit-index)
+    (cond 
+      [(= (length list-of-numbers) 1) (first list-of-numbers)]
+      [else 
+        (define-values (ones zeros) (partition (pred-builder bit-index) list-of-numbers))
+        (define new-index (sub1 bit-index))
+        (cond
+          [(first-pred (length ones) (length zeros)) (rating ones new-index)]
+          [(second-pred (length ones) (length zeros)) (rating zeros new-index)]
+          )])))
+
+(define (oxygen-rating list-of-numbers bit-index)
+  (cond 
+    [(= (length list-of-numbers) 1) (first list-of-numbers)]
+    [else 
+      (define-values (ones zeros) (partition (pred-builder bit-index) list-of-numbers))
+      (define new-index (sub1 bit-index))
+      (cond
+        [(>= (length ones) (length zeros)) (oxygen-rating ones new-index)]
+        [(< (length ones) (length zeros)) (oxygen-rating zeros new-index)]
+        )]))
 
 (define (co2-rating list-of-numbers bit-index)
-  (println (format "called co2 ~a ~a" list-of-numbers bit-index))
-  (cond [(= (length list-of-numbers) 1) (first list-of-numbers)]
-        [else (begin
-                (define-values (ones zeros) (partition (lambda (n) (bitwise-bit-set? n bit-index))
-                                                       list-of-numbers))
-                (cond
-                  [(= (length ones) (length zeros)) 
-                   (begin
-                     (println (format "recurse on ones: ~a" (numbers_10->numbers_2 ones)))
-                     (co2-rating zeros (sub1 bit-index)))]
-                  [(< (length ones) (length zeros))
-                   (begin
-                     (println (format "recurse on ones: ~a" (numbers_10->numbers_2 ones)))
-                     (co2-rating ones (sub1 bit-index)))]
-                  [(> (length ones) (length zeros))
-                   (begin
-                     (println (format "recurse on zeros ~a" (numbers_10->numbers_2 zeros)))
-                     (co2-rating zeros (sub1 bit-index)))]       
-
-                  ))]
-        ) 
-  )
+  (cond 
+    [(= (length list-of-numbers) 1) (first list-of-numbers)]
+    [else 
+      (define-values (ones zeros) (partition (pred-builder bit-index) list-of-numbers))
+      (define new-index (sub1 bit-index))
+      (cond
+        [(< (length ones) (length zeros)) (co2-rating ones new-index)]
+        [(>= (length ones) (length zeros)) (co2-rating zeros new-index)]
+        )]))
 
 (define (solver)
   (define lines (port->lines (current-input-port)))
   (define numbers (map (lambda (l) (string->number l 2)) lines))
   (define oxygen (oxygen-rating numbers MAX-BIT-INDEX))
   (define co2 (co2-rating numbers MAX-BIT-INDEX))
-  (* oxygen co2)
-  )
+  (* oxygen co2))
 
 (with-input-from-file "./input.txt"
                       solver
