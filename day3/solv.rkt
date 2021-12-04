550f4f8aa5153fbab02fb47d0cf4443e4f551291
Michael Moritsugu <mmoritsugu@gmail.com>
2021-12-03 17:46:49 -0800

    day 3 part 1 solution

--- day3/solv.rkt
+++ day3/solv.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(define NUM-BITS 12)
+
 (define (solver)
   (define lines (port->lines (current-input-port)))
   (define numbers (map (lambda (l) (string->number l 2)) lines))
@@ -9,9 +11,16 @@
   (hash-set! ones-count 2 0)
   (hash-set! ones-count 3 0)
   (hash-set! ones-count 4 0)
+  (hash-set! ones-count 5 0)
+  (hash-set! ones-count 6 0)
+  (hash-set! ones-count 7 0)
+  (hash-set! ones-count 8 0)
+  (hash-set! ones-count 9 0)
+  (hash-set! ones-count 10 0)
+  (hash-set! ones-count 11 0)
 
   (for/list ([number (in-list numbers)])
-       (for/list ([bit-index (in-range 0 5)])
+       (for/list ([bit-index (in-range 0 NUM-BITS)])
             (if (bitwise-bit-set? number bit-index)
               (hash-set! ones-count bit-index (add1 (hash-ref ones-count bit-index)))
               void
@@ -19,32 +28,25 @@
        )
 
   (define zeros-count (make-hash))
-
-  (for ([bit-index (in-range 0 5)])
+  (for ([bit-index (in-range 0 NUM-BITS)])
        (hash-set! zeros-count bit-index (- (length numbers) (hash-ref ones-count bit-index))))
-  (println (format "ones ~a" ones-count))
-  (println (format "zeros ~a" zeros-count))
 
   (define gamma 0)
   (define epsilon 0)
-  (for ([bit-index (in-range 0 5)])
+  (for ([bit-index (in-range 0 NUM-BITS)])
        (define val (expt 2 bit-index))
        (if (> (hash-ref zeros-count bit-index)
               (hash-ref ones-count bit-index))
-           (begin 
-             (println (format "setting epsilon bit ~a to ~a" bit-index val))
-						 (set! epsilon (bitwise-ior epsilon val)))
-           (begin
-             (println (format "setting gamma bit ~a to ~a" bit-index val))
-						 (set! gamma (bitwise-ior gamma val)))
+						 (set! epsilon (bitwise-ior epsilon val))
+						 (set! gamma (bitwise-ior gamma val))
          ) 
        )
 
-  (println (format "gamma ~a" gamma))
+  (println (format "gamma: ~a" gamma))
   (println (format "epsilon ~a" epsilon))
   (println (format "solution: ~a" (* gamma epsilon)))
   )
 
-(with-input-from-file "./test-input.txt"
+(with-input-from-file "./input.txt"
                       solver
                       #:mode 'text)
